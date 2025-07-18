9208bf4b5f83fc31b0db795fb28009ecf26787e5
LaerteDelmondes <delmondesoliveiralaerte@gmail.com>
2025-07-18 10:40:00 -0300

    Confirmar devolver jogo

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ function alterarStatus(id) {
     let botao = gameClicado.querySelector('.dashboard__item__button');
 
     if(imagem.classList.contains('dashboard__item__img--rented')) {
+
+        let confirmar = confirm("Você realmente quer devolver este jogo?");
+        if (!confirmar) return;
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'Alugar';
@@ -12,4 +15,10 @@ function alterarStatus(id) {
         botao.textContent = 'Devolver';
         botao.classList.add('dashboard__item__button--return');
     }
+    imprimirJogosAlugados();
+}
+
+function imprimirJogosAlugados() {
+    let alugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    console.log(`Quantidade de jogos alugados: ${alugados}`);
 }
